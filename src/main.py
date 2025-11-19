95b8c416577185f1b7f53e6272fcadf4368182cc
Carmouze Guilhem <57716752+guilhem0908@users.noreply.github.com>
2025-11-19 12:07:46 +0100

    Enhance track selection with error handling
    Added error handling for track selection and integrated dummy path generation.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from pathlib import Path
-
 from ui.process_pygame import process_pygame
-
+from core.process_path import generate_dummy_path
 
 TRACKS = ["small_track.csv", "hairpins_increasing_difficulty.csv", "peanut.csv"]
 
@@ -14,6 +13,17 @@ if __name__ == "__main__":
     print("1 - small_track")
     print("2 - hairpins_increasing_difficulty")
     print("3 - peanut")
-    track_choice = int(input("Your choice (1/2/3): ").strip())
 
-    process_pygame(data_dir / TRACKS[track_choice - 1])
+    try:
+        choice_str = input("Your choice (1/2/3): ").strip()
+        track_choice = int(choice_str)
+        if 1 <= track_choice <= 3:
+            selected_track = data_dir / TRACKS[track_choice - 1]
+
+            my_path = generate_dummy_path()
+
+            process_pygame(selected_track, path=my_path)
+        else:
+            print("Invalid choice.")
+    except ValueError:
+        print("Please enter a number.")
